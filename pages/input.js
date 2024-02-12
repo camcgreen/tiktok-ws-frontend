0d95a3850c40e91ad93890ba2f63e144f6a5790c
camcgreen <c.c.green@outlook.com>
2024-02-12 17:17:26 +0000

    Project set-up

---
+++ pages/input.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
+
+function Input() {
+  const [message, setMessage] = useState('')
+  const ws = useRef(null)
+
+  useEffect(() => {
+    ws.current = new WebSocket('ws://localhost:3000')
+    return () => {
+      ws.current.close()
+    }
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+    console.log('sending message', message)
+    ws.current.send(message)
+    setMessage('')
+  }
+
+  return (
+    <form onSubmit={sendMessage}>
+      <input
+        type='text'
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type='submit'>Send</button>
+    </form>
+  )
+}
+
+export default Input
