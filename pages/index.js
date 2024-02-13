b01825f80f553ece3b686a5944e93c9d318d2410
camcgreen <c.c.green@outlook.com>
2024-02-13 14:53:13 +0000

    Add design and batch script to run

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import Head from 'next/head'
 
 function Home() {
   const [message, setMessage] = useState([])
@@ -10,6 +11,7 @@ function Home() {
     ws.current = new WebSocket(process.env.NEXT_PUBLIC_WS_URL)
     ws.current.onopen = () => {
       console.log('Connected to server')
+      console.log(message)
     }
     ws.current.onmessage = (event) => {
       console.log(event)
@@ -20,7 +22,28 @@ function Home() {
     }
   }, [])
 
-  return <h1>{message}</h1>
+  return (
+    <>
+      <Head>
+        <title>TikTokFashion</title>
+      </Head>
+      <main className='w-screen h-screen-sm'>
+        <section className='w-full flex justify-between p-20 text-4xl text-white '>
+          <h2 className='font-semibold'>#TikTokFashion</h2>
+          <h3 className='text-end'>
+            competition powered by
+            <br />
+            <span className='font-thin'>S H E E R L U X E</span>
+          </h3>
+        </section>
+        <section className='absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 w-1/2 py-8 px-8 bg-white rounded-full flex justify-center'>
+          <h1 className='text-center font-medium text-4xl'>
+            {message && message.length > 0 ? message : '@TikTokHandle'}
+          </h1>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default Home
