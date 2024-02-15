acb7afd94a8122fa7adf116022ff5620a1eb125a
FelixChirinosLopez <felixchirinoslopez@gmail.com>
2024-02-15 13:26:25 +0000

    added animations and new background change

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
 
+let characterArray = "@TikTokHandle".split("");
+
+let backgorund1State = true;
 function Home() {
-  const [message, setMessage] = useState([]);
+  const [background1, setBackground1] = useState("/images/Asset 2spiral.png");
+  const [background2, setBackground2] = useState("/images/placeholder.jpg");
+  const [showBackground1, setShowBackground1] = useState(true);
+
+  const [message, setMessage] = useState("@TikTokHandle");
   const ws = useRef(null);
 
   useEffect(() => {
@@ -14,13 +21,72 @@ function Home() {
       console.log(message);
     };
     ws.current.onmessage = (event) => {
-      console.log(event);
-      setMessage(event.data);
+      switch (event.data) {
+        case "change_background 1":
+          backgorund1State == false && setShowBackground1(true);
+          backgorund1State = true;
+          break;
+        case "change_background 2":
+          backgorund1State == true && setShowBackground1(false);
+          backgorund1State = false;
+
+          break;
+
+        default:
+          console.log(event);
+          console.log(characterArray.length);
+
+          //characterArray = message.split("");
+          CharactersOneByOne(event.data);
+          // characterArray.forEach((el, index) => {
+          //   console.log(el + " " + index);
+          // });
+
+          // setMessage(event.data);
+          break;
+      }
     };
     return () => {
       ws.current.close();
     };
   }, []);
+
+  function CharactersOneByOne(newMessage) {
+    let index = 0;
+    let tempArray = characterArray;
+    const intervalId1 = setInterval(() => {
+      if (index < tempArray.length) {
+        tempArray.splice(index, 1); // Remove the printed character from the array
+        const temptemp = tempArray.toString().replace(/,/g, "");
+        console.log(temptemp);
+        setMessage(temptemp);
+        index == tempArray.length && setMessage("");
+        // No need to increment index since splice() already removes the current character
+      } else {
+        // tempArray = temptemp;
+        setMessage("");
+
+        clearInterval(intervalId1); // Stop the interval when all characters are printed
+        //characterArray = tempArray;
+        //setMessage(newMessage);
+
+        let sourceString = newMessage;
+        let appendString = "";
+        let indexx = 0;
+        const intervalId2 = setInterval(() => {
+          if (indexx < sourceString.length) {
+            appendString += sourceString[indexx]; // Concatenate one character from appendString
+            console.log(appendString); // Print the updated string
+            indexx++;
+            setMessage(appendString);
+          } else {
+            characterArray = newMessage.split("");
+            clearInterval(intervalId2); // Stop the interval when all characters are appended
+          }
+        }, 100); // Append one character every 100 milliseconds
+      }
+    }, 100); // Print one character every second (1000 milliseconds)
+  }
 
   return (
     <>
@@ -40,9 +106,22 @@ function Home() {
           className="absolute left-0 top-0 w-screen h-screen-sm object-cover"
         /> */}
         <img
-          src="/Asset 2spiral.png"
+          src={background1}
           alt="Background"
-          className="absolute left-0 top-0 w-screen h-screen-sm object-cover"
+          style={{
+            transform: `translateX(${showBackground1 ? "0%" : "100%"})`,
+            transition: "opacity 0.5s ease-in-out, transform 0.5s ease-in-out",
+          }}
+          className={`absolute left-0 top-0 w-screen h-screen-sm object-cover`}
+        />
+        <img
+          src={background2}
+          style={{
+            transform: `translateX(${showBackground1 ? "-100%" : "0%"})`,
+            transition: "opacity 0.5s ease-in-out, transform 0.5s ease-in-out",
+          }}
+          alt="Background"
+          className={`absolute left-0 top-0 w-screen h-screen-sm object-cover`}
         />
         <section className="relative w-full flex justify-between p-20 text-4xl text-white ">
           <h2 className="font-semibold">#TikTokFashion</h2>
@@ -53,8 +132,12 @@ function Home() {
           </h3>
         </section>
         <section className="absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2 w-1/2 py-8 px-8 bg-white rounded-full flex justify-center">
-          <h1 className="text-center font-medium text-4xl">
-            {message && message.length > 0 ? message : "@TikTokHandle"}
+          <h1
+            className="text-center font-medium text-4xl"
+            style={{ height: "60px" }}
+          >
+            {message}
+            {/* message && message.length > 0 ? message : "@TikTokHandle" */}
           </h1>
         </section>
       </main>
--- pages/input.js
+++ pages/input.js
@@ -1,74 +1,118 @@
-import { useEffect, useRef, useState } from 'react'
-import Head from 'next/head'
+import { useEffect, useRef, useState } from "react";
+import Head from "next/head";
 
 function Input() {
-  const [message, setMessage] = useState('')
-  const [showToast, setShowToast] = useState(false)
-  const ws = useRef(null)
+  const [message, setMessage] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  const ws = useRef(null);
 
   useEffect(() => {
     // ws.current = new WebSocket('ws://localhost:3000')
-    ws.current = new WebSocket(process.env.NEXT_PUBLIC_WS_URL)
+    ws.current = new WebSocket(`ws://${process.env.NEXT_PUBLIC_WS_URL}`);
     return () => {
-      ws.current.close()
-    }
-  }, [])
+      ws.current.close();
+    };
+  }, []);
 
   const sendMessage = (event) => {
-    event.preventDefault()
-    console.log('sending message', message)
-    ws.current.send(message)
-    setShowToast(true)
-    setMessage('')
+    event.preventDefault();
+    console.log("sending message", message);
+    ws.current.send(message);
+    setShowToast(true);
+    setMessage("");
     setTimeout(() => {
-      setShowToast(false)
-    }, 1500)
-  }
+      setShowToast(false);
+    }, 1500);
+  };
+
+  const changeBackground = (Background) => {
+    event.preventDefault();
+    console.log("sending message", message);
+    ws.current.send("change_background " + Background);
+    setShowToast(true);
+    setMessage("");
+    setTimeout(() => {
+      setShowToast(false);
+    }, 1500);
+  };
 
   return (
     <>
       <Head>
         <title>TikTokFashion</title>
       </Head>
-      <main className='w-screen h-screen-sm'>
-        <section className='w-full flex justify-between p-20 text-2xl text-white '>
-          <h2 className='font-semibold'>#TikTokFashion</h2>
-          <h3 className='text-end'>
+
+      <main className="w-screen h-screen-sm">
+        {" "}
+        <img
+          src="/images/Asset 2spiral.png"
+          alt="Background"
+          className="absolute left-0 top-0 w-screen h-screen-sm object-cover"
+          style={{ zIndex: "-10" }}
+        />
+        <section className="w-full flex justify-between p-20 text-2xl text-white ">
+          <h2 className="font-semibold">#TikTokFashion</h2>
+          <h3 className="text-end">
             competition powered by
             <br />
-            <span className='font-thin'>S H E E R L U X E</span>
+            <span className="font-thin">S H E E R L U X E</span>
           </h3>
         </section>
-        <section className='w-3/4 absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2'>
+        <section className="w-3/4 absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2">
           {/* <h1 className='text-center font-medium text-4xl'>@TikTokHandle</h1> */}
-          <form onSubmit={sendMessage} className='flex'>
+          <form onSubmit={sendMessage} className="flex">
             <input
-              className='py-8 px-8 rounded-full bg-white text-center font-medium text-2xl flex-grow mr-8'
-              type='text'
+              className="py-8 px-8 rounded-full bg-white text-center font-medium text-2xl flex-grow mr-8"
+              type="text"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
             <button
-              className='px-16 py-8 bg-black text-center font-medium text-2xl text-white rounded-full'
-              type='submit'
+              className="px-16 py-8 bg-black text-center font-medium text-2xl text-white rounded-full"
+              type="submit"
             >
               Send
             </button>
           </form>
         </section>
         <div
-          className='fixed right-1/2 top-4 translate-x-1/2 px-8 py-4 bg-white rounded-full transition-all'
+          className="fixed right-1/2 top-4 translate-x-1/2 px-8 py-4 bg-white rounded-full transition-all"
           style={{
             transform: showToast
-              ? 'translate(50%, 0)'
-              : 'translate(50%, -100px)',
+              ? "translate(50%, 0)"
+              : "translate(50%, -100px)",
           }}
         >
-          {'✅ - Message sent'}
+          {"✅ - Message sent"}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "0",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+            width: "70%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <button
+            className="px-16 py-8 bg-white text-center font-medium text-2xl text-black rounded-full"
+            onClick={() => changeBackground(1)}
+          >
+            Backgorund 1
+          </button>
+          <button
+            className="px-16 py-8 bg-white text-center font-medium text-2xl text-black rounded-full"
+            onClick={() => changeBackground(2)}
+          >
+            Backgorund 2
+          </button>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default Input
+export default Input;
